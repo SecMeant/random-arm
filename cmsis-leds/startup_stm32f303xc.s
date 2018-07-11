b8c8e02ca7228fe870fc9a8356c28d30f9003e77
secmeant <patryk.wlazlyn@gmail.com>
2018-07-11 16:17:01 +0200

    Further bootloaders code optimalization

--- cmsis-leds/startup_stm32f303xc.s
+++ cmsis-leds/startup_stm32f303xc.s
@@ -92,14 +92,14 @@ LoopCopyDataInit:
 	cmp	r2, r3
 	bcc	CopyDataInit
 	ldr	r2, =_sbss
+	ldr	r3, = _ebss
 	b	LoopFillZerobss
 /* Zero fill the bss segment. */
 FillZerobss:
-	movs	r3, #0
-	str	r3, [r2], #4
+	movs	r1, #0
+	str	r1, [r2], #4
 
 LoopFillZerobss:
-	ldr	r3, = _ebss
 	cmp	r2, r3
 	bcc	FillZerobss
 
