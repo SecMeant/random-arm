9db0fe9e13a579adab7666eb5b63cb5c644fbb98
secmeant <patryk.wlazlyn@gmail.com>
2018-07-12 15:00:24 +0200

    Adding circleTC with BSRR method

--- hardfault-handler/Src/main.c
+++ hardfault-handler/Src/main.c
@@ -5,15 +5,22 @@ void delay(int t);
 void circle();
 void circleT(int T);
 void circleTC(int T, int C);
+void circleTC_BSRR(int T, int C);
 void flashT(int T);
 void invalidInstr(){asm volatile (".word 0xf7f0a000\n");}
 
 // Interrupt handlers
 void HardFault_Handler(void);
 
+void initGPIO(void);
+
+#define PINS_MODER 0x55550000
+
 int main(void)
 {
-	circleTC(75000, 32);
+	initGPIO();
+
+	circleTC_BSRR(50000, 32);
 
 	// Causes hardfault handler to be invoked
 	invalidInstr();
@@ -23,12 +30,6 @@ void circleT(int T)
 {
 	// Used for turning on leds
 	unsigned leds = 0x00000100;
-
-	// Enables GPIOE ??
-	RCC->AHBENR |= (1<<21);
-
-	// ????
-	GPIOE->MODER |= 0x55550000;
 
 	for(;;)
 	{
@@ -53,12 +54,6 @@ void circleTC(int T, int C)
 	// Used for turning on leds
 	unsigned leds = 0x00000100;
 
-	// Enables GPIOE ??
-	RCC->AHBENR |= (1<<21);
-
-	// ????
-	GPIOE->MODER |= 0x55550000;
-
 	for(int _c=0; _c<C; _c++)
 	{
 		delay(T);
@@ -66,6 +61,29 @@ void circleTC(int T, int C)
 		// 0x0000XX00 --> XX is 8 bit long and describes states
 		// if 8 leds on board. 1 - set, 0 - reset
 		GPIOE->ODR = leds;
+
+		leds <<= 1;
+
+		if((leds>>16) != 0)
+		{
+			leds &= 0x0000ff00;
+			leds |= 0x00000100;
+		}
+	}
+}
+
+void circleTC_BSRR(int T, int C)
+{
+	// Used for turning on leds
+	unsigned leds = 0x00000100;
+
+	for(int _c=0; _c<C; _c++)
+	{
+		delay(T);
+		// Sets leds on
+		// 0x0000XX00 --> XX is 8 bit long and describes states
+		// if 8 leds on board. 1 - set, 0 - reset
+		GPIOE->BSRR = leds | 0xffff0000;
 
 		leds <<= 1;
 
@@ -86,12 +104,6 @@ void flashT(int T)
 {
 	// Used for turning on leds
 	unsigned leds = 0x0000ff00;
-
-	// Enables GPIOE ??
-	RCC->AHBENR |= (1<<21);
-
-	// ????
-	GPIOE->MODER |= 0x55550000;
 
 	for(;;)
 	{
@@ -118,4 +130,13 @@ void HardFault_Handler(void)
   {
 		flashT(200000);
   }
+}
+
+void initGPIO(void)
+{
+	// Enables GPIOE ??
+	RCC->AHBENR = (1<<21);
+
+	// ????
+	GPIOE->MODER |= PINS_MODER;
 }
