b6b7f41cb724380675acba4f62939e47d9bbc463
secmeant <patryk.wlazlyn@gmail.com>
2018-07-20 01:11:47 +0200

    Adding example on basic timers

--- shared/Inc/stm32hal.h
+++ shared/Inc/stm32hal.h
@@ -40,4 +40,63 @@ struct RCC_
 	unsigned CFGR3;
 }*RCC = (RCC_*)(0x40021000);
 
+struct ADTIM
+{
+	unsigned CR1;
+	unsigned CR2;
+	unsigned SMCR;
+	unsigned DIER;
+	unsigned SR;
+	unsigned EGR;
+	unsigned CCMR1;
+	unsigned CCMR2;
+	unsigned CCER;
+	unsigned CNT;
+	unsigned PSC;
+	unsigned ARR;
+	unsigned RCR;
+	unsigned CCR1;
+	unsigned CCR2;
+	unsigned CCR3;
+	unsigned CCR4;
+	unsigned BDTR;
+	unsigned DCR;
+	unsigned DMAR;
+	unsigned OR;
+	unsigned CCMR3;
+	unsigned CCR5;
+	unsigned CCR6;
+}*TIM1 = (ADTIM*)0x40012C00, *TIM8 = (ADTIM*)0x40013400, *TIM20 = (ADTIM*)0x40015000;
+
+struct BCTIM
+{
+	unsigned CR1;
+	unsigned CR2;
+	unsigned RESERVED1;
+	unsigned DIER;
+	unsigned SR;
+	unsigned EGR;
+	unsigned RESERVED2;
+	unsigned CNT;
+	unsigned PSC;
+	unsigned ARR;
+
+	void reset(void);
+
+	void init(void);
+}*TIM6 = (BCTIM*)0x40001000, *TIM7 = (BCTIM*)0x40001400;
+
+
+void BCTIM::reset(void)
+{
+	CR1 = 0;
+	CR2 = 0;
+	DIER = 0;
+	SR = 0;
+	EGR = 0;
+	CNT = 0;
+	PSC = 0;
+	ARR = 0xFFFF;
+}
+
 #endif //STM32HAL_H
---
+++ timv1/Src/main.c
@@ -0,0 +1,222 @@
+/* THIS PROGRAM WILL ONLY MAKE SENSE IF U CONNECT 
+ * PC0 AND PA0 PINS TOGETHER. CLICK USER BUTTON 
+ * TO MAKE LEDS SHINE BRIGHT !*/
+
+#include "stm32hal.h"
+
+extern "C"
+{
+// Helper functions
+void delay(int t);
+void circle();
+void circleT(int T);
+void circleTC(int T, int C, GPIO* port = GPIOE);
+void circleTC_BSRR(int T, int C);
+void flashT(int T);
+bool isB1Clicked(void);
+bool isC0ON(void);
+void invalidInstr(){asm volatile (".word 0xf7f0a000\n");}
+
+// Interrupt handlers
+void HardFault_Handler(void);
+
+void initGPIO(void);
+void initADTIM(void);
+void initBCTIM(void);
+void startTIM6(void);
+void startTIM1(void);
+int getCounter(BCTIM *tim);
+}
+
+#define PINS_MODER 0x55550000
+
+int main(void)
+{
+	initGPIO();
+	initBCTIM();
+	startTIM1();
+	
+	int cnt;
+	while(1)
+	{
+		if(isB1Clicked())
+		{
+			cnt = getCounter(TIM6);
+
+			if(TIM6->SR & 1)
+				GPIOE->ODR = 0x0000ff00;
+		}
+	}
+}
+
+void circleT(int T)
+{
+	// Used for turning on leds
+	unsigned leds = 0x00000100;
+
+	for(;;)
+	{
+		delay(T);
+		// Sets leds on
+		// 0x0000XX00 --> XX is 8 bit long and describes states
+		// if 8 leds on board. 1 - set, 0 - reset
+		GPIOE->ODR = leds;
+
+		leds <<= 1;
+
+		if((leds>>16) != 0)
+		{
+			leds &= 0x0000ff00;
+			leds |= 0x00000100;
+		}
+	}
+}
+
+void circleTC(int T, int C, GPIO* port)
+{
+	// Used for turning on leds
+	unsigned leds = 0x00000100;
+
+	for(int _c=0; _c<C; _c++)
+	{
+		delay(T);
+		// Sets leds on
+		// 0x0000XX00 --> XX is 8 bit long and describes states
+		// if 8 leds on board. 1 - set, 0 - reset
+		port->ODR = leds;
+
+		leds <<= 1;
+
+		if((leds>>16) != 0)
+		{
+			leds &= 0x0000ff00;
+			leds |= 0x00000100;
+		}
+	}
+}
+
+void circleTC_BSRR(int T, int C)
+{
+	// Used for turning on leds
+	unsigned leds = 0x00000100;
+
+	for(int _c=0; _c<C; _c++)
+	{
+		delay(T);
+		// Sets leds on
+		// 0x0000XX00 --> XX is 8 bit long and describes states
+		// if 8 leds on board. 1 - set, 0 - reset
+		GPIOE->BSRR = leds | 0xffff0000;
+
+		leds <<= 1;
+
+		if((leds>>16) != 0)
+		{
+			leds &= 0x0000ff00;
+			leds |= 0x00000100;
+		}
+	}
+}
+
+void circle()
+{
+	circleT(75000);
+}
+
+void flashT(int T)
+{
+	// Used for turning on leds
+	unsigned leds = 0x0000ff00;
+
+	for(;;)
+	{
+		delay(T);
+		GPIOE->ODR = leds;
+		delay(T);
+		GPIOE->ODR = 0;
+	}
+}
+
+void delay(int t)
+{
+	volatile int i,j;
+
+	for(i=0; i < t; i++)
+	{
+		j++;
+	}
+}
+
+void HardFault_Handler(void)
+{
+  while (1)
+  {
+		flashT(200000);
+  }
+}
+
+void initGPIO(void)
+{
+	// Enables clock for GPIOE, GPIOC and GPIOA
+	RCC->AHBENR  = (1<<21);
+	RCC->AHBENR |= (1<<19);
+	RCC->AHBENR |= (1<<17);
+
+	// Sets E port pins to output mode
+	GPIOE->MODER = 0x55550000;
+
+	// Sets C port pins to input mode
+	GPIOC->MODER = 0x00000000;
+
+	// Sets A port pins to input mode ( reset value)
+	GPIOA->MODER = 0xA8000000;
+
+	// nopull mode ( reset value )
+	GPIOA->PUPDR = 0x64000000;
+
+	// pull-down mode
+	GPIOC->PUPDR = 0xAAAAAAAA;
+}
+
+void IM(void)
+{
+	// enable TIM1
+	RCC->APB2ENR |= (1<<11);
+}
+
+void startTIM1(void)
+{
+	TIM1->CR1 |= 1;
+}
+
+void initBCTIM(void)
+{
+	// TIM6 enable
+	RCC->APB1ENR |= (1<<4);
+
+	TIM6->CR1 |= 0x888;
+	TIM6->DIER |= 1;
+	TIM6->EGR = 1;
+	TIM6->PSC = 0;
+	TIM6->ARR = 0x000f;
+}
+
+void startTIM6(void)
+{
+	TIM6->CR1 |= 1;
+}
+
+int getCounter(BCTIM *tim)
+{
+	return tim->CNT & 0x0000ffff;
+}
+
+bool isB1Clicked(void)
+{
+	return GPIOA->IDR & 0x00000001;
+}
+
+bool isC0ON(void)
+{
+	return GPIOC->IDR & 0x00000001;
+}
